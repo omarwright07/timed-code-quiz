6f1e86f50a0f81944424c28db6373f3a8f81e35f
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-03-31 12:44:48 -0500

    quiz can be played til timer runs out, all buttons have click events, and content can be swap

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,16 @@
 // Header
 var viewHighScoreEl = document.querySelector("#view-high-score");
 var timeEl = document.querySelector("#timer");
-var timerValue = 3;
+var timerMax = 3;
+var timerValue = 0;
 var myTimer = 0;
 // Content
 var pageContentEl = document.querySelector("#content");
-var contentIntroEl = document.querySelector("#intro");
-var contentQuestionEl = document.querySelector("#question");
-var contentHighScoreSubmitEl = document.querySelector("#high-score-submit");
-var contentHighScoreDisplayEl = document.querySelector("#high-score-display");
-var buttonStartQuiz = document.querySelector("#btn-start-quiz");
-// Footer
-var responseEl = document.querySelector("#response");
 // Object that holds array of every question property
 var questions = [
     {
-        question: 0,
-        answer1: 0,
-        answer2: 0,
-        answer3: 0,
-        answer4: 0,
+        question: "test",
+        answers: ["test1", "test2", "test3", "test4"],
         correctAnswer: 0,
     }
 ];
@@ -29,100 +20,154 @@ var highScores = [];
 
 // ############################################################
 // ############################################################
-
-// Creation Functions
-var createQuestion = function () {
-    // Create article
-    var articleQuestionEl = document.createElement("article");
-    articleQuestionEl.className = "art-question";
-    articleQuestionEl.id = "question";
-    // All the tags inside with content
-    articleQuestionEl.innerHTML = '<h1>Commonly used data types DO NOT Include?</h1><ol><button type="button"><li>"Start Quiz"</li></button><button type="button"><li>Start Quiz</li></button><button type="button"><li>Start Quiz</li></button><button type="button"><li>Start Quiz</li></button></ol>';
-    // Appends to section for content
-    pageContentEl.appendChild(articleQuestionEl);
+var clearContent = function () {
+    var contentIntroEl = document.querySelector("#intro");
+    var contentQuestionEl = document.querySelector("#question");
+    var contentHighScoreSubmitEl = document.querySelector("#high-score-submit");
+    var contentHighScoreDisplayEl = document.querySelector("#high-score-display");
+    if (contentIntroEl != null) {
+        contentIntroEl.remove();
+    }
+    if (contentQuestionEl != null) {
+        contentQuestionEl.remove();
+    }
+    if (contentHighScoreSubmitEl != null) {
+        contentHighScoreSubmitEl.remove();
+    }
+    if (contentHighScoreDisplayEl != null) {
+        contentHighScoreDisplayEl.remove();
+    }
 }
 
-// var createArticle = function (className,idName) {
-//     var articleQuestionEl = document.createElement("article");
-//     articleQuestionEl.className = className;
-//     articleQuestionEl.id = idName;
-// }
+var clearFooter = function () {
+    var contentResponseEl = document.querySelector("#response");
+    if (contentResponseEl != null) {
+        contentResponseEl.remove();
+    }
+}
+
+// Element Creation Functions -------------------------------------------
+var createArticle = function (className, idName) {
+    var article = document.createElement("article");
+    article.className = className;
+    article.id = idName;
+    // Appends to section for content
+    pageContentEl.appendChild(article);
+    return article;
+}
+
+var createIntro = function () {
+    // Create article
+    var articleIntroEl = createArticle("art-general center", "intro");
+    // All the tags inside with content
+    articleIntroEl.innerHTML = '<h1>Coding Quiz Challenge</h1><p>Try to answer the following code-related questions within the time limit. Keep in mind that ansers will penalize your score/time by ten seconds!</p><button type="button" id="btn-start-quiz">Start Quiz</button>';
+}
+
+var createQuestion = function () {
+    // Create article
+    var articleQuestionEl = createArticle("art-question", "question");
+    // All the tags inside with content
+    articleQuestionEl.innerHTML = '<h1>' + questions[0].question + '</h1><ol><button type="button" id="answer"><li id="answer">' + questions[0].answers[0] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[0].answers[1] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[0].answers[2] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[0].answers[3] + '</li></button></ol>';
+}
 
 var createHighScoreSubmit = function () {
     // Create article
-    var articleHighSubmitEl = document.createElement("article");
-    articleHighSubmitEl.className = "art-question";
-    articleHighSubmitEl.id = "high-score-submit";
+    var articleHighSubmitEl = createArticle("art-question", "high-score-submit");
     // All the tags inside with content
-    articleHighSubmitEl.innerHTML = '<h1>All done!</h1><h2>Your final score is 22.</h2><div class="initials-entry"><label for="initials">Enter initials:</label><input type="text" name="initials"><br><br><span><button type="button">Submit</button></span></div>';
-    // Appends to section for content
-    pageContentEl.appendChild(articleHighSubmitEl);
-    // .addEventListener("submit",);
+    articleHighSubmitEl.innerHTML = '<h1>All done!</h1><h2>Your final score is ' + timerValue + '.</h2><div class="initials-entry"><label for="initials">Enter initials:</label><input type="text" name="initials"><br><br><span><button type="button"  id="submit">Submit</button></span></div>';
 }
 
 var createHighScoreDisplay = function () {
-    contentIntroEl.remove();
+    clearContent();
+    clearFooter();
     // Create article
-    var articleHighDisplayEl = document.createElement("article");
-    articleHighDisplayEl.className = "art-general";
-    articleHighDisplayEl.id = "high-score-display";
-// All the tags inside with content
-    articleHighDisplayEl.innerHTML = '<h1>High scores</h1><div class="high-score-entry"><ol><li>AB - 22</li><li>AB - 22</li></ol></div><div><button type="button">Go back</button><button type="button">Clear high scores</button></div>';
-    // Appends to section for content
-    pageContentEl.appendChild(articleHighDisplayEl);
+    var articleHighDisplayEl = createArticle("art-general", "high-score-display");
+    // All the tags inside with content
+    articleHighDisplayEl.innerHTML = '<h1>High scores</h1><div class="high-score-entry"><ol><li>AB - 22</li><li>AB - 22</li></ol></div><div><button type="button" id="go-back">Go back</button><button type="button" id="clear-high-scores">Clear high scores</button></div>';
 }
 
 var createQuestionResponse = function () {
-    var footerResponseEl = document.createElement("footer");
-    footerResponseEl.className = "art-question"
-    footerResponseEl.id = "response";
-    // change wrong to a variable and seperate into another function to called
+    var footerResponseEl = document.querySelector("#response");
+    if (footerResponseEl == null) {
+        footerResponseEl = document.createElement("footer");
+        footerResponseEl.className = "art-question"
+        footerResponseEl.id = "response";
+        document.querySelector("body").appendChild(footerResponseEl);
+    }
     footerResponseEl.innerHTML = "<h1>Wrong!</h1>";
-    document.querySelector("body").appendChild(footerResponseEl);
 }
 
 // Trigger Functions
 // Timer Functions -------------------------------------------
 var timerSet = function () {
-    console.log("Set timer to: " + timerValue);
+    timeEl.innerHTML = "Timer: " + timerMax;
+    timerValue = timerMax;
+}
+
+var timerUpdate = function () {
     timeEl.innerHTML = "Timer: " + timerValue;
 }
+
 var timerStart = function () {
-    if (timerValue <= 0) {
+    if (timerValue <= 1) {
+        timerValue--;
+        timerUpdate();
         gameOver();
     } else {
         timerValue--;
-        timerSet();
+        timerUpdate();
     }
+}
+
+// Start/End/Save Functions -------------------------------------------
+var startQuiz = function () {
+    timerSet();
+    myTimer = setInterval(timerStart, 1000);
+    clearContent();
+    createQuestion();
+}
+
+var gameOver = function () {
+    clearInterval(myTimer);
+    clearContent();
+    createHighScoreSubmit();
 }
 
 var saveHighScore = function () {
     // localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// Start/End Functions -------------------------------------------
-var startQuiz = function () {
-    console.log("Starting Quiz");
-    console.log("Starting Timer");
-    myTimer = setInterval(timerStart, 1000);
-    // Removes Intro
-    contentIntroEl.remove();
-    createQuestion();
-    createQuestionResponse();
+// Handlers all button clicks
+var buttonHandler = function (event) {
+    var targetEl = event.target.getAttribute("id");
+    console.log(targetEl);
+    switch (targetEl) {
+        case "btn-start-quiz":
+            startQuiz();
+            break;
+        case "answer":
+            createQuestionResponse();
+            break;
+        case "submit":
+            createHighScoreDisplay();
+            break;
+        case "go-back":
+            clearContent();
+            createIntro();
+            break;
+        case "clear-high-scores":
+            console.log("Clearing High Scores");
+            break;
+        default:
+    }
 }
 
-var gameOver = function () {
-    console.log("Game Over!!");
-    clearInterval(myTimer);
-    console.log("Timer Stopped");
-    var highScore = timerValue;
-    console.log("Your high score is: " + highScore);
-}
 // ############################################################
 // ############################################################
 
 // "Click" Event Listeners
-buttonStartQuiz.addEventListener("click", startQuiz);
+pageContentEl.addEventListener("click", buttonHandler);
 viewHighScoreEl.addEventListener("click", createHighScoreDisplay);
+// .addEventListener("submit",);
 
-timerSet();
+createIntro();
