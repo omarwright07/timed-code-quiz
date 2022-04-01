0d81933a5f7f7ed3ed53d333aafb12f9e18b5d20
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-03-31 23:49:02 -0500

    add the ability to enter initials and check for empty entries

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // Header
 var viewHighScoreEl = document.querySelector("#view-high-score");
 var timeEl = document.querySelector("#timer");
-var timerMax = 3;
+var timerMax = 75;
 var timerValue = 0;
 var myTimer = 0;
 // Content
@@ -10,11 +10,17 @@ var pageContentEl = document.querySelector("#content");
 // Object that holds array of every question property
 var questions = [
     {
-        question: "test",
+        question: "test question 1",
         answers: ["test1", "test2", "test3", "test4"],
-        correctAnswer: 0,
+        correctAnswer: "test1",
+    },
+    {
+        question: "test question 2",
+        answers: ["test1", "test2", "test3", "test4"],
+        correctAnswer: "test4",
     }
 ];
+var questionIndex = 0;
 // Array holds all the high scores to be stored and displayed
 var highScores = [];
 
@@ -60,14 +66,22 @@ var createIntro = function () {
     // Create article
     var articleIntroEl = createArticle("art-general center", "intro");
     // All the tags inside with content
-    articleIntroEl.innerHTML = '<h1>Coding Quiz Challenge</h1><p>Try to answer the following code-related questions within the time limit. Keep in mind that ansers will penalize your score/time by ten seconds!</p><button type="button" id="btn-start-quiz">Start Quiz</button>';
+    articleIntroEl.innerHTML = '<h1>Coding Quiz Challenge</h1><p>Try to answer the following code-related questions within the time limit. Keep in mind that answers will penalize your score/time by ten seconds!</p><button type="button" id="btn-start-quiz">Start Quiz</button>';
 }
 
 var createQuestion = function () {
-    // Create article
-    var articleQuestionEl = createArticle("art-question", "question");
-    // All the tags inside with content
-    articleQuestionEl.innerHTML = '<h1>' + questions[0].question + '</h1><ol><button type="button" id="answer"><li id="answer">' + questions[0].answers[0] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[0].answers[1] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[0].answers[2] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[0].answers[3] + '</li></button></ol>';
+    var articleQuestionEl = document.querySelector("#question");
+    if (articleQuestionEl == null) {
+        // Create article
+        articleQuestionEl = createArticle("art-question", "question");
+    }
+    if (questionIndex >= questions.length) {
+        questionIndex = 0;
+        gameOver();
+    } else {
+        // All the tags inside with content
+        articleQuestionEl.innerHTML = '<h1>' + questions[questionIndex].question + '</h1><ol><button type="button" id="answer"><li id="answer">' + questions[questionIndex].answers[0] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[questionIndex].answers[1] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[questionIndex].answers[2] + '</li></button><button type="button" id="answer"><li id="answer">' + questions[questionIndex].answers[3] + '</li></button></ol>';
+    }
 }
 
 var createHighScoreSubmit = function () {
@@ -86,7 +100,7 @@ var createHighScoreDisplay = function () {
     articleHighDisplayEl.innerHTML = '<h1>High scores</h1><div class="high-score-entry"><ol><li>AB - 22</li><li>AB - 22</li></ol></div><div><button type="button" id="go-back">Go back</button><button type="button" id="clear-high-scores">Clear high scores</button></div>';
 }
 
-var createQuestionResponse = function () {
+var createQuestionResponse = function (stringResponse) {
     var footerResponseEl = document.querySelector("#response");
     if (footerResponseEl == null) {
         footerResponseEl = document.createElement("footer");
@@ -94,7 +108,7 @@ var createQuestionResponse = function () {
         footerResponseEl.id = "response";
         document.querySelector("body").appendChild(footerResponseEl);
     }
-    footerResponseEl.innerHTML = "<h1>Wrong!</h1>";
+    footerResponseEl.innerHTML = "<h1>" + stringResponse + "</h1>";
 }
 
 // Trigger Functions
@@ -108,6 +122,11 @@ var timerUpdate = function () {
     timeEl.innerHTML = "Timer: " + timerValue;
 }
 
+var timerSubtract = function () {
+    timerValue = timerValue - 10;
+    timerUpdate();
+}
+
 var timerStart = function () {
     if (timerValue <= 1) {
         timerValue--;
@@ -119,7 +138,7 @@ var timerStart = function () {
     }
 }
 
-// Start/End/Save Functions -------------------------------------------
+// Misc Functions -------------------------------------------
 var startQuiz = function () {
     timerSet();
     myTimer = setInterval(timerStart, 1000);
@@ -133,11 +152,66 @@ var gameOver = function () {
     createHighScoreSubmit();
 }
 
+var checkAnswer = function (event) {
+    if (event.innerHTML == questions[questionIndex].correctAnswer) {
+        createQuestionResponse("Correct!");
+    } else {
+        createQuestionResponse("Wrong!");
+        timerSubtract();
+    }
+    questionIndex++;
+    createQuestion();
+}
+
+// Create a hide and show "view high score"
+
+
+
 var saveHighScore = function () {
-    // localStorage.setItem("tasks", JSON.stringify(tasks));
+    // localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+var createHighScoreEntry = function (inputInitials) {
+    var highScoreEntry = {
+        initials: inputInitials,
+        score: timerValue,
+    };
+    highScores.push(highScoreEntry);
+    console.log(highScores);
+}
+
+
+
+var loadHighScore = function () {
+    // var savedHighScores = localStorage.getItem("highScores");
+    // savedHighScores = JSON.parse(savedHighScores);
+    // "<li>" + highScoresEntry[i].initials + "-" + highScoresEntry[i].initials + "</li>";
+    // loop through savedHighScores array
+    // for (var i = 0; i < savedTasks.length; i++) {
+    //     createHighScoreEntry(savedHighScores[i]);
+    // }
+}
+
+// Create a clear high score function
+var clearHighScore = function () {
+    console.log("Clearing High Scores");
+    highScores = [];
+    console.log(highScores);
+    saveHighScore();
 }
 
 // Handlers all button clicks
+var submitHandler = function () {
+    var inputInitials = document.querySelector("input[name='initials']").value;
+    if (!inputInitials) {
+        alert("You need to enter your initials!");
+        return false;
+    }
+    createHighScoreEntry(inputInitials);
+    createHighScoreDisplay();
+}
+
+
 var buttonHandler = function (event) {
     var targetEl = event.target.getAttribute("id");
     console.log(targetEl);
@@ -146,17 +220,17 @@ var buttonHandler = function (event) {
             startQuiz();
             break;
         case "answer":
-            createQuestionResponse();
+            checkAnswer(event.target);
             break;
         case "submit":
-            createHighScoreDisplay();
+            submitHandler();
             break;
         case "go-back":
             clearContent();
             createIntro();
             break;
         case "clear-high-scores":
-            console.log("Clearing High Scores");
+            clearHighScore();
             break;
         default:
     }
@@ -171,3 +245,4 @@ viewHighScoreEl.addEventListener("click", createHighScoreDisplay);
 // .addEventListener("submit",);
 
 createIntro();
+console.log(highScores);
